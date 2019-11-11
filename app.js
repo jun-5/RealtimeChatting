5a2262933b375f12f8296e8abdb225288af3faf5
rkd7997 <rkd7997@coinbit.co.kr>
2019-11-11 20:40:20 +0900

    기본 폴더구조 세팅

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
+const fs = require('fs');
 
-app.get('/', function(req, res){
-    console.log('유저가 /으로 접속하였습니다.!');
-    res.send('Hello, Express Server~!!!~!!');
-});
+
+app.use('/css', express.static('./static/css'))
+
+app.use('/js', express.static('./static/js'))
+
+
+app.get('/', function(request, response) {
+    fs.readFile('./static/index.html', function(err, data) {
+      if(err) {
+        response.send('에러')
+      } else {
+        response.writeHead(200, {'Content-Type':'text/html'})
+        response.write(data)
+        response.end()
+      }
+    })
+  })
 
 server.listen(8080, function(){
     console.log('서버실행중');
