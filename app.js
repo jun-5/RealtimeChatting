226ece23682419b583dbe97fe0660f07a0234e98
rkd7997 <rkd7997@coinbit.co.kr>
2019-11-11 21:55:47 +0900

    채팅서비스 구현완료

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ app.get('/', function(request, response) {
     })
   })
 
+  io.sockets.on('connection', function(socket){
+
+      socket.on('newUser', function(name){
+          console.log(name + "님이 접속하셨습니다.");
+          socket.name = name;
+          io.sockets.emit('update' ,{type: 'connect', name: 'SERVER', message: name+'님이 접속하셨습니다.'})
+      });
+
+      socket.on('message', function(data){
+          data.name =socket.name;
+          console.log(data.name + ' : ' + data.message);
+          socket.broadcast.emit('update',data);
+      })
+
+
+      socket.on('disconnect', function(){
+          console.log(socket.name +'님이 나가셨습니다.');
+          socket.broadcast.emit('update', {type:'disconnect', name:'SERVER',message:socket.name +'님이 나가셨습니다.'});
+      })
+    })
+
 server.listen(8080, function(){
     console.log('서버실행중');
 });
